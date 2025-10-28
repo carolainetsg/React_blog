b157a1ec4ea091983239eed3261602b325a9350e
Carolaine <carolainetsg98@gmail.com>
2025-10-28 20:08:01 -0300

    Página Login!

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -2,21 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
+import {AuthProvider} from "./contexts/AuthContext";
 
 
 function App(){
   return(
     <>
+    <AuthProvider>
     <BrowserRouter>
     <Navbar />
     <div className= "min-h-[80vh]">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </div>
     <Footer />
     </BrowserRouter>   
+    </AuthProvider>
     </>
   );
 }
